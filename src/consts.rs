b21700b74fd2b6c27dac51643e7e865007475073
Dex <sheepareawesome777@gmail.com>
2022-10-31 17:00:26 -0400

    Split `main.rs` into modules

---
+++ src/consts.rs
@@ -0,0 +1,5 @@
+// Constants for the app
+
+pub	const TITLEBAR_HEIGHT:	f32		= 24.0;
+pub	const APP_NAME_STR:		&str	= "Derecrypt";
+pub	const DC_VERSION:		&str	= env!("CARGO_PKG_VERSION");
--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 //#![feature(hash_drain_filter)]
 
-use std::{fs, path::Path, collections::HashMap};
-use tinyfiledialogs as tfd;
+use std::fs;
 use eframe::{egui::{*, style::Widgets}};
-use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
 
-const TITLEBAR_HEIGHT:	f32		= 24.0;
-const APP_NAME_STR:		&str	= "Derecrypt";
-const DC_VERSION:		&str	= env!("CARGO_PKG_VERSION");
+// Keep all the definition stuff in a separate file to
+// make it easier to read this one
+mod consts;
+use consts::*;
 
-struct ThemeColors;
+mod classes;
+use classes::*;
 
-impl ThemeColors {
-	const BG_PURPLE:		Color32	= Color32::from_rgb(79,	0,	148);
-	const BG_PURPLE_DEEP:	Color32	= Color32::from_rgb(42,	0,	79);
-	const BG_PURPLE_LIGHT:	Color32	= Color32::from_rgb(142,	24,	240);
-	const BG_PURPLE_DARK:	Color32	= Color32::from_rgb(18,	0,	33);
-	const TEXT:				Color32 = Color32::WHITE;
-}
 
 fn main() {
 	let options = eframe::NativeOptions {
@@ -41,100 +33,6 @@ fn main() {
 		options,
 		Box::new(|_cc| Box::new(Derecrypt::new())),
 	);
-}
-
-#[derive(Eq, Hash, PartialEq, Clone, EnumIter)]
-enum WindowTypes {
-	ConvertBase,
-	Replace,
-}
-
-// DcM is short for DerecryptModule
-// Got tired of writing verbose struct/enum/variable names,
-// so if you see "DcM" in the code, just think of the app's modules
-struct DcM {
-	active:		bool,
-}
-
-struct Derecrypt {
-	open_modals:	HashMap<WindowTypes, DcM>,
-	outfile:		Option<String>,
-	string:			String,
-}
-
-impl Derecrypt {
-	pub fn new() -> Self {
-		let mut modals_map = HashMap::new();
-
-		// initialize all the modules
-		for wintype in WindowTypes::iter() {
-			modals_map.insert(wintype, DcM {
-				active:		false,
-			});
-		}
-
-		Derecrypt {
-			open_modals:	modals_map,
-			outfile:		None,
-			string:			String::new(),
-		}
-	}
-
-	pub fn filename(&self) -> Option<String> {
-		let outfile = &self.outfile.as_ref();
-
-		if let Some(v) = outfile {
-			Some(Path::new(v).file_name().unwrap().to_str().unwrap().to_string())
-		} else {
-			None
-		}
-	}
-
-	
-	pub fn toggle_module_visibility(&mut self, wintype: WindowTypes) {
-		self.open_modals.entry(wintype)
-			.and_modify(|val| val.active = !val.active);
-	}
-
-	// Pops up a dialog to open a new file, and then asks
-	// if the selected path should be the new output path
-	fn get_desired_path(&mut self, save: bool, force_overwrite: bool) -> String {
-		let fname;
-
-		loop {
-			let chosen = if save {
-				tfd::save_file_dialog(
-					"Save To File", ""
-				)
-			} else {
-				tfd::open_file_dialog(
-					"Load String From File", "", None
-				)
-			};
-
-			if chosen.is_some() {
-				fname = chosen.unwrap();
-				break;
-			} else {
-				tfd::message_box_ok(
-					APP_NAME_STR,
-					"Invalid File! Please specify a file to open.",
-					tfd::MessageBoxIcon::Error
-				);
-			};
-		}
-
-		if self.outfile.is_none() || force_overwrite || tfd::message_box_yes_no(
-			APP_NAME_STR,
-			format!("Replace the current working path with {}?", &fname[..]).as_str(),
-			tfd::MessageBoxIcon::Question,
-			tfd::YesNo::Yes
-		) == tfd::YesNo::Yes {
-			self.outfile = Some(fname.clone());
-		};
-
-		fname
-	}
 }
 
 impl eframe::App for Derecrypt {
@@ -198,8 +96,6 @@ impl eframe::App for Derecrypt {
 					}
 				}
 			}
-			
-			ui.heading("Arguments");
 
 			ui.horizontal(|ui| {
 				if ui.button("Strip").clicked() {
