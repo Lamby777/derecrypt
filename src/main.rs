4bd16963ba33f6f12b84f1145f7557a05c95d6d7
Dex <sheepareawesome777@gmail.com>
2022-11-04 08:20:04 -0400

    Error handling + Use `u128` for maximum size

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 
 use std::{mem, fs};
 use eframe::{egui::{*, style::Widgets}};
+use tinyfiledialogs as tfd;
 
 // Keep all the definition stuff in a separate file to
 // make it easier to read this one
@@ -94,11 +95,42 @@ impl eframe::App for Derecrypt {
 
 								// Run module
 								if dcm_run(ui) {
-									let res = u32::from_str_radix(
+									// If "from" not in range, set to binary
+									if !(2..=36).contains(from) {
+										*from = 2;
+									}
+
+									// If string empty,
+
+									let res = u128::from_str_radix(
 										&self.string, *from
-									).unwrap();
-									
-									self.string = res.to_string();
+									);
+
+									match res {
+										Ok(v) => {
+											self.string = v.to_string();
+										},
+
+										Err(v) => match v.kind() {
+											core::num::IntErrorKind::PosOverflow => {
+												tfd::message_box_ok(
+													APP_NAME_STR,
+													"Attempting to calculate this caused\
+													a positive integer overflow.",
+													tfd::MessageBoxIcon::Error
+												);
+											},
+
+											_ => {
+												tfd::message_box_ok(
+													APP_NAME_STR,
+													"Number is invalid for this base!\n\
+													Did you mean to use the ASCII module?",
+													tfd::MessageBoxIcon::Error
+												);
+											}
+										}
+									}
 								}
 							});
 					},
