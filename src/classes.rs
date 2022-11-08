306f5bd10883b3e9fff50946e994e529ec14145e
Dex <sheepareawesome777@gmail.com>
2022-11-08 02:11:08 -0500

    Moved color consts to `consts.rs`

--- src/classes.rs
+++ src/classes.rs
@@ -3,22 +3,11 @@
 */
 
 use tinyfiledialogs as tfd;
-use eframe::{egui::*};
 use strum::IntoEnumIterator;
 use strum_macros::{EnumIter, EnumDiscriminants};
 use std::{path::Path, collections::HashMap};
 
 use super::consts::*;
-
-pub struct ThemeColors;
-
-impl ThemeColors {
-	pub const BG_PURPLE:		Color32	= Color32::from_rgb(79,	0,	148);
-	pub const BG_PURPLE_DEEP:	Color32	= Color32::from_rgb(42,	0,	79);
-	pub const BG_PURPLE_LIGHT:	Color32	= Color32::from_rgb(142,	24,	240);
-	pub const BG_PURPLE_DARK:	Color32	= Color32::from_rgb(18,	0,	33);
-	pub const TEXT:				Color32 = Color32::WHITE;
-}
 
 pub mod win_s {
     use super::WindowTypes;
