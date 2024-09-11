8e81ff884c343cb3644585d38ebe3dcea8531743
Cherry <cherry@sparklet.org>
2024-09-11 17:21:52 -0400

    top bar shows current buffer path

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,18 @@ use consts::{APP_ID, DC_VERSION};
 use gtk::glib::Propagation;
 mod modules;
 
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 
 use adw::gtk::{ApplicationWindow, CssProvider};
 use adw::prelude::*;
 use adw::{glib, Application};
 use gtk::gio::Cancellable;
-use gtk::{gdk, EventControllerKey, Overflow, TextView};
+use gtk::{gdk, EventControllerKey, Label, Overflow, TextView};
+
+static DC: RwLock<Option<Derecrypt>> = RwLock::new(None);
 
 /// Program state
 pub struct Derecrypt {
-    pub open_modals: Vec<()>, // TODO
-
     pub outfile: Option<String>,
     pub string: Arc<Mutex<String>>,
 }
@@ -28,7 +28,6 @@ pub struct Derecrypt {
 impl Default for Derecrypt {
     fn default() -> Self {
         Self {
-            open_modals: vec![],
             outfile: None,
             string: Arc::new(Mutex::new(String::new())),
         }
@@ -36,6 +35,13 @@ impl Default for Derecrypt {
 }
 
 fn main() -> glib::ExitCode {
+    // initialize the app state
+    {
+        let mut dc = DC.write().unwrap();
+        *dc = Some(Derecrypt::default());
+    }
+
+    // start the gtk app
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_window);
     app.connect_startup(|_| load_css());
@@ -64,8 +70,7 @@ fn build_window(app: &Application) {
         .clone();
 
     // Present window
-    let textview = build_main_ui(&window);
-    window.present();
+    let (textview, outfile_label) = build_main_ui(&window);
 
     let key_controller = EventControllerKey::new();
     let window2 = window.clone();
@@ -75,7 +80,7 @@ fn build_window(app: &Application) {
         let shift_down = state.contains(gdk::ModifierType::SHIFT_MASK);
 
         if ctrl_down && keyval.to_lower() == gdk::Key::o {
-            open_file_dialog(&window2, &textview2);
+            open_file_dialog(&window2, &textview2, &outfile_label, !shift_down);
 
             return Propagation::Stop;
         }
@@ -86,23 +91,25 @@ fn build_window(app: &Application) {
     // Add the key event controller to the window
     window.add_controller(key_controller);
 
+    window.present();
     window.connect_close_request(|_| glib::Propagation::Proceed);
 }
 
-fn build_main_ui(window: &ApplicationWindow) -> TextView {
+fn build_main_ui(window: &ApplicationWindow) -> (TextView, Label) {
     let main_box = gtk::Box::builder()
         .hexpand(true)
         .orientation(gtk::Orientation::Vertical)
         .build();
 
     let (textbox, textview) = build_text_box();
-    let top_row = build_top_row(window, &textview);
+    let (top_row, outfile_label) = build_top_row(window, &textview);
 
     main_box.append(&top_row);
+    main_box.append(&gtk::Separator::new(gtk::Orientation::Horizontal));
     main_box.append(&textbox);
 
     window.set_child(Some(&main_box));
-    textview
+    (textview, outfile_label)
 }
 
 fn build_text_box() -> (gtk::ScrolledWindow, gtk::TextView) {
@@ -127,36 +134,95 @@ fn build_text_box() -> (gtk::ScrolledWindow, gtk::TextView) {
     (scroll, textview)
 }
 
-fn open_file_dialog(window: &ApplicationWindow, textbox: &TextView) {
+/// Open a file dialog to select a file to open
+fn open_file_dialog(
+    window: &ApplicationWindow,
+    textbox: &TextView,
+    outfile_label: &Label,
+    overwrite_outfile: bool,
+) {
     let dialog = gtk::FileDialog::builder().build();
+
     let textbox2 = textbox.clone();
+    let outfile_label2 = outfile_label.clone();
     dialog.open(Some(window), None::<&Cancellable>, move |file| {
         let Ok(file) = file else {
             println!("No file selected.");
             return;
         };
+
         let path = file.path().unwrap();
         let content = std::fs::read_to_string(&path).unwrap();
+
         textbox2.buffer().set_text(&content);
+        if overwrite_outfile {
+            let path_str = path.to_str().unwrap().to_string();
+
+            let mut dc = DC.write().unwrap();
+            dc.as_mut().unwrap().outfile = Some(path_str);
+            outfile_label2
+                .set_label(&outfile_fmt(&dc.as_ref().unwrap().outfile));
+        }
     });
 }
 
-fn build_top_row(window: &ApplicationWindow, textbox: &TextView) -> gtk::Box {
+fn build_top_row(
+    window: &ApplicationWindow,
+    textbox: &TextView,
+) -> (gtk::Box, Label) {
+    let dc = DC.read().unwrap();
+    let dc = dc.as_ref().unwrap();
+
     let top_row = gtk::Box::builder()
         .orientation(gtk::Orientation::Horizontal)
+        // .homogeneous(true)
+        .build();
+
+    // make the file menu buttons
+    let buttons_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .margin_end(100)
         .build();
 
     let cast_button = gtk::Button::builder().label("Cast").build();
     let open_button = gtk::Button::builder().label("Open").build();
     let save_button = gtk::Button::builder().label("Save").build();
 
+    buttons_box.append(&cast_button);
+    buttons_box.append(&open_button);
+    buttons_box.append(&save_button);
+
+    // make the output path label
+    let outfile_label = gtk::Label::builder()
+        .label(outfile_fmt(&dc.outfile))
+        .name("outfile_label")
+        .build();
+
+    // make the 3rd box (idk what it's used for yet)
+    let box3 = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .build();
+
+    // connect the button signals
     let window2 = window.clone();
     let textbox2 = textbox.clone();
-    open_button.connect_clicked(move |_| open_file_dialog(&window2, &textbox2));
+    let outfile_label2 = outfile_label.clone();
+    open_button.connect_clicked(move |_| {
+        open_file_dialog(&window2, &textbox2, &outfile_label2, true)
+    });
 
-    top_row.append(&cast_button);
-    top_row.append(&open_button);
-    top_row.append(&save_button);
+    top_row.append(&buttons_box);
+    top_row.append(&outfile_label);
+    top_row.append(&box3);
 
-    top_row
+    (top_row, outfile_label)
+}
+
+fn outfile_fmt(outfile: &Option<String>) -> String {
+    let path = outfile
+        .as_ref()
+        .map(|v| v.to_string())
+        .unwrap_or("<none>".to_string());
+
+    format!("Output Path: {}", path)
 }
