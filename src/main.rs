5ac6e464a9310998ceb5f01ecd587afae5628e64
Cherry <cherry@sparklet.org>
2024-09-11 01:23:08 -0400

    add "open" button

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@
 
 use std::sync::{Arc, Mutex};
 
-use adw::Application;
-use gtk::prelude::*;
-use gtk::{glib, ApplicationWindow, CssProvider};
+use adw::gtk::{ApplicationWindow, CssProvider};
+use adw::prelude::*;
+use adw::{glib, Application};
 
 mod consts;
 use consts::{APP_ID, DC_VERSION};
+use gtk::gio;
 mod modules;
 
 /// Program state
@@ -25,9 +26,9 @@ pub struct Derecrypt {
     pub string: Arc<Mutex<String>>,
 }
 
-impl Derecrypt {
-    pub fn new() -> Self {
-        Derecrypt {
+impl Default for Derecrypt {
+    fn default() -> Self {
+        Self {
             open_modals: vec![],
             outfile: None,
             string: Arc::new(Mutex::new(String::new())),
@@ -57,8 +58,8 @@ fn build_window(app: &Application) {
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
-        .width_request(400)
-        .height_request(300)
+        .width_request(800)
+        .height_request(600)
         .title(format!("derecrypt v{}", DC_VERSION))
         .build()
         .clone();
@@ -71,11 +72,44 @@ fn build_window(app: &Application) {
 }
 
 fn build_main_ui(window: &ApplicationWindow) {
-    let main_box = gtk::Box::builder().hexpand(true).build();
+    let main_box = gtk::Box::builder()
+        .hexpand(true)
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+
+    let top_row = top_row(window);
 
     let text_view =
         gtk::TextView::builder().hexpand(true).vexpand(true).build();
+
+    main_box.append(&top_row);
     main_box.append(&text_view);
 
     window.set_child(Some(&main_box));
+}
+
+fn top_row(window: &ApplicationWindow) -> gtk::Box {
+    let top_row = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .build();
+
+    let cast_button = gtk::Button::builder().label("Cast").build();
+    let open_button = gtk::Button::builder().label("Open").build();
+    let save_button = gtk::Button::builder().label("Save").build();
+
+    let window2 = window.clone();
+    open_button.connect_clicked(move |_| {
+        let dialog = gtk::FileDialog::builder().build();
+        let cancellable = gio::Cancellable::new();
+
+        dialog.open(Some(&window2), Some(&cancellable), |file| {
+            let _ = dbg!(file);
+        });
+    });
+
+    top_row.append(&cast_button);
+    top_row.append(&open_button);
+    top_row.append(&save_button);
+
+    top_row
 }
